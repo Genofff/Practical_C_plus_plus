494e81b70ec6138ab9466302ae2b26f0db5b882e
Genofff <114654769+Genofff@users.noreply.github.com>
2023-10-24 17:48:35 +0300

    Update example_6.cpp

--- P_4/example_6.cpp
+++ P_4/example_6.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int main(void)
+int main()
 {
     bool answer;
     int value;
@@ -8,5 +8,4 @@ int main(void)
     cin >> value;
     answer = value >= 0 && value < 10 || value * 2 < 20 && value - 2 > -2 || value - 1 > 1 && value / 2 < 10 || value == 111;
     cout << (answer ? "THAT'S TRUE :)" : "THAT'S NOT TRUE :(") << endl;
-    return 0;
 }
